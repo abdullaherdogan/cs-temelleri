739f7c03a559de1b314bb879ba03af5bcce22b24
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-13 20:49:33 +0300

    C# first homework updated

---
+++ console-programming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Isminizi girin!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin");
+            string surName= Console.ReadLine();
+            Console.WriteLine("isminiz "+ name+" Soyisminiz " + surName);
+        }
+    }
+}
