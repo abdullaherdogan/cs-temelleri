b21638b3014098632dd5046d767af8a4b385f2af
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-13 22:33:18 +0300

    class homework uploaded

---
+++ basic-class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace basic_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker hasan = new Worker();
+            hasan.Name = "Hasan";
+            hasan.Surname = "Kavakli";
+            hasan.Age = 29;
+            hasan.WorkerInfo();
+            Worker mehmet = new Worker();
+            mehmet.Name = "Mehmet";
+            mehmet.Surname = "Subasi";
+            mehmet.Age = 22;
+            mehmet.WorkerInfo(); 
+        }
+
+        class Worker
+        {
+            public string Name;
+            public string Surname;
+
+            public int Age;
+
+            public void WorkerInfo(){
+                Console.WriteLine("Name: "+ Name+" Surname: "+ Surname);
+            }
+        }
+    }
+}
