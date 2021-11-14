741bd4a7415066ce99bd8f50813868a899e03a29
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-14 21:24:38 +0300

    encapsulation homework uploaded

---
+++ encapsulation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ferhat = new Student("Ferhat","Aslan",1);
+            ferhat.Downgrade();
+            ferhat.StudentInfo();
+        }
+    }
+}
+
+class Student
+{
+    private string name;
+    private string surname;
+    private int yearOfSchool;
+
+    public string Name {get=> name;set=>name = value;}
+    public string Surname { get=>surname;set=>surname=value; }
+    public int YearOfSchool
+    {
+        get=> yearOfSchool;
+        set
+        {
+            if (value<1)
+            {
+                Console.WriteLine("Year of school can not be less than 1");
+                yearOfSchool=1;
+            }
+            else
+            {
+                yearOfSchool=value;
+            }
+        }
+    }
+
+    public Student(string _name,string _surname,int _yearOfSchool)
+    {
+        Name = _name; // atamaları get set verdiğimiz proplara yapıyoruz
+        Surname = _surname;
+        YearOfSchool = _yearOfSchool;
+    }
+    public void Downgrade()
+    {
+        this.YearOfSchool = this.YearOfSchool-1; // propu değiştiriyoruz
+    }
+    public void StudentInfo()
+    {
+        Console.WriteLine("Name:   "+name);
+        Console.WriteLine("Surname:   "+surname);
+        Console.WriteLine("Year of School:   "+yearOfSchool);
+
+    }
+}
