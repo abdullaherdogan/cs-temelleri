663fc357b14cdff1eba2e912a83a0dc271e88567
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-14 20:34:03 +0300

    Constructors Homework uploaded

---
+++ constructors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker merve = new Worker("Merve","Yaman","mrv@mail.com");
+            merve.WorkerInfo();
+        }
+    }
+}
+
+class Worker
+{
+    public string Name;
+    public string Surname;
+    public string Mail;
+
+    public Worker(string _Name,string _Surname,string _Mail)
+    {
+        this.Name = _Name;
+        this.Surname = _Surname;
+        this.Mail = _Mail;
+    }
+    public void WorkerInfo()
+    {
+        Console.WriteLine("Name: "+ Name+" Surname: "+Surname);
+    }
+}
