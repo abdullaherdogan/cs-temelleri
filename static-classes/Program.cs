925034ef3e606d0780c00d0e11a61823ba52d2dd
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-15 21:43:48 +0300

    static classes homework uploaded

---
+++ static-classes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace static_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker hasan = new Worker("Hasan","Baltacı");
+            Console.WriteLine(Worker.TotalPaid);
+            Worker mehmet = new Worker("Mehmet","Halit");
+            Console.WriteLine(Worker.TotalPaid);
+            
+        }
+    }
+}
+
+class Worker
+{
+    private static int _totalPaid;
+    public static int TotalPaid{get=>_totalPaid;}
+
+    private string _name;
+    private string _surname;
+
+    public string Name
+    {
+        get{
+            return _name;
+        }
+        set{
+            _name=value;
+        }
+    }
+    public string Surname
+    {
+        get{
+            return _surname;
+        }
+        set{
+            _surname=value;
+        }
+    }
+    static Worker()
+    {
+        _totalPaid = 0;
+    }
+    public Worker(string name,string surname)
+    {
+        this.Name= name;
+        this.Surname=surname;
+        _totalPaid += 2800;
+    }
+}
