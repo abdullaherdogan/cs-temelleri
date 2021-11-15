6154bceed02c8127256734b0da33913d85fbff42
Abdullah Erdoğan <erdogan15077@gmail.com>
2021-11-15 22:50:20 +0300

    enums homework uploaded

---
+++ enums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Monday);
+            Console.WriteLine((int)Days.Friday);// index of Friday
+
+            int Temprature= 32;
+            if (Temprature>(int)Weather.Normal)
+            {
+                Console.WriteLine("It's a little hot outside");
+            }
+        }
+    }
+}
+
+enum Weather
+{
+    Cold=5,
+    Normal=20,
+    Hot = 29,
+    RealyHot=35
+}
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
